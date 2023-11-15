8ab9ad05aa87231559f1affc327fb6690c362b48
Mark Nispel <mnispel@gmail.com>
2023-11-15 05:12:36 -0600

    add capability to send a second message.

--- TxUdp.py
+++ TxUdp.py
@@ -9,7 +9,7 @@ UDP_PORT = 49153
 
 #CONSTRUCT A IONetworkControlMessage
 MESSAGE = bytearray([0x55,0xAA,0x00,0xff,0xaa,0x55,0xff,0x00])  # sync pattern
-MESSAGE += bytearray([0xff,0xff]) # msgCommand
+MESSAGE += bytearray([0x00,0x02]) # msgCommand
 MESSAGE += bytearray([14]) # number of data bytes in this message after the header
 MESSAGE += bytearray([1]) #message format version
 MESSAGE += bytearray([0x00,0x00,]) # message security number
@@ -23,4 +23,21 @@ print("message: %s" % MESSAGE)
 
 sd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sd.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+#CONSTRUCT a second IONetworkControlMessage
+MESSAGE = bytearray([0x55,0xAA,0x00,0xff,0xaa,0x55,0xff,0x00])  # sync pattern
+MESSAGE += bytearray([0xff,0xff]) # msgCommand
+MESSAGE += bytearray([14]) # number of data bytes in this message after the header
+MESSAGE += bytearray([1]) #message format version
+MESSAGE += bytearray([0x00,0x00,]) # message security number
+MESSAGE += bytearray([0x00,0x00]) # reserved 1
+MESSAGE += bytearray([0x00,0x00]) # msg verification value (CRC)
+MESSAGE += bytearray([0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, 0x00]) # msg data
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+
+#sd.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
 sd.close()
