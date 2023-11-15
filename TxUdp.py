bf24824be12e409694c68d0785c5580eca15767a
marknispel <97249172+marknispel@users.noreply.github.com>
2023-11-15 07:30:57 -0600

    enable two message Tx.

--- TxUdp.py
+++ TxUdp.py
@@ -9,7 +9,7 @@ UDP_PORT = 49153
 
 #CONSTRUCT A IONetworkControlMessage
 MESSAGE = bytearray([0x55,0xAA,0x00,0xff,0xaa,0x55,0xff,0x00])  # sync pattern
-MESSAGE += bytearray([0x00,0x02]) # msgCommand
+MESSAGE += bytearray([0xff,0xff]) # msgCommand
 MESSAGE += bytearray([14]) # number of data bytes in this message after the header
 MESSAGE += bytearray([1]) #message format version
 MESSAGE += bytearray([0x00,0x00,]) # message security number
